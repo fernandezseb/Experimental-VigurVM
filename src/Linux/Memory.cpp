33cb2fb861c744a5b714e04cb87185de1d145b61
Sebastiaan Fernandez <fernandezsebastiaan@gmail.com>
2024-01-01 21:22:53 +0100

    Fix Memory class on Linux

--- src/Linux/Memory.cpp
+++ src/Linux/Memory.cpp
@@ -4,7 +4,7 @@ Memory::Memory(size_t size, size_t maxSize)
 	: m_size(size), m_maxSize(maxSize)
 {
 	m_size = maxSize;
-	memoryPtr = (uint8_t*)Platform::allocateMemory(m_maxSize, 0);
+	m_memoryPtr = (uint8_t*)Platform::allocateMemory(m_maxSize, 0);
 }
 
 Memory::~Memory()
