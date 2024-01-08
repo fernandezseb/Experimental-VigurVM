b6cc2306cc96375cd5697f73b9941a7a89820dc3
Sebastiaan Fernandez <fernandezsebastiaan@gmail.com>
2024-01-08 21:05:43 +0100

    Fix native function linkage

--- src/Library/java/io/FileDescriptor.cpp
+++ src/Library/java/io/FileDescriptor.cpp
@@ -15,7 +15,7 @@
 
 #include "FileDescriptor.h"
 
- void lib_java_io_FileDescriptor_initIDs(NATIVE_ARGS)
+JCALL void lib_java_io_FileDescriptor_initIDs(NATIVE_ARGS)
 {
     printf("[Running initIDs from FileDescriptor]\n");
 }
--- src/Library/java/io/FileOutputStream.cpp
+++ src/Library/java/io/FileOutputStream.cpp
@@ -15,7 +15,7 @@
 
 #include "FileOutputStream.h"
 
-void lib_java_io_FileOutputStream_initIDs(NATIVE_ARGS)
+JCALL void lib_java_io_FileOutputStream_initIDs(NATIVE_ARGS)
 {
     printf("[Running initIDs from FileOutputStream]\n");
 }
