fb81fed0345328ff353a3d1265b9a6903af11136
Sebastiaan Fernandez <fernandezsebastiaan@gmail.com>
2025-12-17 21:45:28 +0100

    Refactor Configuration

--- src/VM/Configuration.h
+++ src/VM/Configuration.h
@@ -1,7 +1,8 @@
-
 #pragma once
+
 #include <string_view>
 #include <vector>
+#include <span>
 
 struct Configuration
 {
@@ -9,4 +10,38 @@ struct Configuration
     std::string_view mainClassName;
     std::vector<std::string_view> args;
     bool disableSystemInit = false;
+
+    static Configuration fromArguments(const std::span<const char*> cargs)
+    {
+        Configuration config;
+        bool mainNameFound = false;
+        for (int currentArg = 1; currentArg < cargs.size(); ++currentArg)
+        {
+            const std::string_view arg{cargs[currentArg]};
+            if (arg  == "-classpath")
+            {
+                if (currentArg+1 < cargs.size())
+                {
+                    config.classPath = std::string_view{cargs[currentArg+1]};
+                    currentArg++;
+                }
+            } else if (arg == "-disableSystemInit")
+            {
+                config.disableSystemInit = true;
+            }
+            else
+            {
+                if (!mainNameFound)
+                {
+                    config.mainClassName = arg;
+                    mainNameFound = true;
+                } else
+                {
+                    config.args.push_back(arg);
+                }
+            }
+        }
+
+        return config;
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -19,45 +19,12 @@
 #include <span>
 #include <string_view>
 
-static Configuration parseArguments(const std::span<const char*> args)
-{
-    Configuration config;
-    bool mainNameFound = false;
-    for (int currentArg = 1; currentArg < args.size(); ++currentArg)
-    {
-        const std::string_view arg{args[currentArg]};
-        if (arg  == "-classpath")
-        {
-            if (currentArg+1 < args.size())
-            {
-                config.classPath = std::string_view{args[currentArg+1]};
-                currentArg++;
-            }
-        } else if (arg == "-disableSystemInit")
-        {
-            config.disableSystemInit = true;
-        }
-        else
-        {
-            if (!mainNameFound)
-            {
-                config.mainClassName = arg;
-                mainNameFound = true;
-            } else
-            {
-                config.args.push_back(arg);
-            }
-        }
-    }
-
-    return config;
-}
 
 int main(const int argc, const char* argv[])
 {
     const size_t size = argc;
     const std::span args{argv, size};
-    const Configuration config = parseArguments(args);
+    const Configuration config = Configuration::fromArguments(args);
 
     VM vm(config);
     vm.start(std::string_view{args[0]});
